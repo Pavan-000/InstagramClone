62220e48139c3019d7bb2048b28c6da45a363bc7
Pavan-000 <epk13@iitbbs.ac.in>
2023-01-16 23:32:03 +0530

    Updates

---
+++ precache-manifest.654010f0d5d585368cc08c3e26406563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4664c2796d121d9f3e5fd05eeca7696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f146fb639b3ec4880ef",
+    "url": "/static/css/main.a4135bf3.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f146fb639b3ec4880ef",
+    "url": "/static/js/main.92ac7043.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
