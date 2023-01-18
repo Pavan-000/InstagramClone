c57e4a2964c8e628c951b6fb983c0bbf018f3181
Pavan-000 <epk13@iitbbs.ac.in>
2023-01-18 10:08:46 +0530

    ten

--- index.js
+++ index.js
@@ -30,18 +30,17 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
-// if(process.env.NODE_ENV=="production"){
-//     app.use(express.static('client/build'))
-//     const path = require('path')
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
 
-app.get("/",async(req,res)=>{
-    console.log(process.env.NODE_ENV,process.env.MONGO_URI)
-    res.send("Welcome to Instagram Server")
-})
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+
+// app.get("/",async(req,res)=>{
+//     console.log(process.env.NODE_ENV,process.env.MONGO_URI)
+//     res.send("Welcome to Instagram Server")
+// })
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
